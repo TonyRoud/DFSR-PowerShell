e60d2d7d6896b03c7d726df6164ec1f58aea5a2d
Anthony Roud <anthoy.roud@amorgroup.local>
2018-10-12 11:46:42 +0100

    added Get-StagingQuotaEstimate

---
+++ Get-StagingQuotaEstimate.ps1
@@ -0,0 +1,34 @@
+# Grab replicated folders and the local folder paths
+$ReplicatedFolder = Get-Dfsreplicatedfolder | Get-DfsrMembership | Where-Object { $_.computername -eq "$env:computername" } | Select-Object foldername, contentpath
+
+Function Get-DfsStagingQuotaEstimate {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true)]$ReplicatedFolder
+    )
+    Foreach ($folder in $ReplicatedFolder) {
+
+        Write-Verbose "`nChecking Folder: $($folder.foldername)"
+        $Top32 = Get-ChildItem $folder.contentpath -recurse | Sort-Object length -descending | select-object -first 32
+
+        Write-Verbose "Top 32 files in directory $($folder.contentpath)"
+        Write-Verbose "`n $Top32 | ft name,length -wrap –auto"
+
+        $Top32Sum = ($Top32 | Measure-Object 'length' -sum).sum
+        $Top32GB = ([math]::truncate($Top32Sum / 1MB))
+        Write-Output "`n`nTotal recommended staging quota size for $($folder.foldername) is $Top32GB MB"
+    }
+}
+
+
+$healthy = @(
+    'PROVISIONING'
+    'CUSTOMIZING'
+    'DELETING'
+    'MAINTENANCE'
+    'PROVISIONED'
+    'CONNECTED'
+    'ISCONNECTED'
+    'AVAILABLE'
+    'DISCONNECTED'
+)
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This module contains functions for monitoring & managing Windows DFSR using PowerShell
 
 These functions build on the existing functionality of the Microsoft DFSR cmdlets and adds further functionality.
+
+Get-StagingQuotaEstimate.ps1 is a script which can help with identifying the top 300 files in a replicated directory, in order to set the staging quota to an appropriate threshold.
